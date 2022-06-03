689e8f20ddac1a23623f937ee5a4174a6eed88ae
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2022-06-02 22:06:18 -0300

    Adicionando a Toolbar ao projeto

--- app/src/main/java/br/com/kevinlucas/android/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/br/com/kevinlucas/android/nybooks/presentation/books/BooksActivity.kt
@@ -3,10 +3,14 @@ package br.com.kevinlucas.android.nybooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.kevinlucas.android.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
     }
 }
