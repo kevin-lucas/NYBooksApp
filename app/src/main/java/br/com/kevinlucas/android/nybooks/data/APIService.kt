f7635088accbc6703e80b616775c1e406dd96f7f
Kevin Lucas <55221269+kevin-lucas@users.noreply.github.com>
2022-06-03 12:48:28 -0300

    Criando e utilizando a camada de network

--- app/src/main/java/br/com/kevinlucas/android/nybooks/data/APIService.kt
+++ app/src/main/java/br/com/kevinlucas/android/nybooks/data/APIService.kt
@@ -11,6 +11,7 @@ object APIService {
     private fun initRetrofit () : Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/svc/books/v3/")
+
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
--- app/src/main/java/br/com/kevinlucas/android/nybooks/data/NYTServices.kt
+++ app/src/main/java/br/com/kevinlucas/android/nybooks/data/NYTServices.kt
@@ -1,13 +1,14 @@
 package br.com.kevinlucas.android.nybooks.data
 
-import br.com.kevinlucas.android.nybooks.data.model.Book
+import br.com.kevinlucas.android.nybooks.data.response.BookBodyResponse
 import retrofit2.Call
 import retrofit2.http.GET
-
-
-
+import retrofit2.http.Query
 
 interface NYTServices {
     @GET("lists.json")
-    fun listRepos(): Call<List<Book>>
+    fun getBooks(
+        @Query("api-key") apiKey: String = "17e1M0SBPhVdgwskXzvmSuAGmPdZmZEA",
+        @Query("list") list: String = "hardcover-fiction"
+    ): Call<List<BookBodyResponse>>
 }
