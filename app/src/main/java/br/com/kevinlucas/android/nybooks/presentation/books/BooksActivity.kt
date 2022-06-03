26d61884dd64b7b85e4c411b8d540379535af3b2
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2022-06-02 21:54:44 -0300

    Criando pacotes e renomeando projeto

---
+++ app/src/main/java/br/com/kevinlucas/android/nybooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package br.com.kevinlucas.android.nybooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.kevinlucas.android.nybooks.R
+
+class BooksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
